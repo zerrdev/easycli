88636084c2a9088c282ff87f1126841b7b40a7e2
zerr <gabriel@zerr.dev>
2026-01-31 16:38:35 +0000

    feat: add config type definitions

---
+++ src/config/types.ts
@@ -0,0 +1,20 @@
+export interface ToolConfig {
+  cmd: string;
+}
+
+export interface GroupConfig {
+  tool: string;
+  restart: 'yes' | 'no' | 'unless-stopped';
+  items: string[];
+}
+
+export interface EasyCliConfig {
+  tools?: Record<string, ToolConfig>;
+  groups: Record<string, GroupConfig>;
+}
+
+export interface ProcessItem {
+  name: string;
+  args: string[];
+  fullCmd: string;
+}
